9811517caddf50f057121b16176b4150f34fadb5
J.O.SHIVNESH <149822585+red-1165@users.noreply.github.com>
2025-03-13 16:36:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "hi, this is task 4\n";
+  cout << "hi, this is lab8\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
